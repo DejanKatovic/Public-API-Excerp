86724a53252cc6db8205bc82052ef6bd883e24ce
chris <elvin0728@protonmail.com>
2022-03-28 16:28:29 +0900

    completed 1100urls

--- src/modules/string.tsx
+++
@@ -1,9 +0,0 @@
-export const getConvertedText = (str: string): string => {
-  const chars = str.split("");
-  const convertedChars = chars.map((character) =>
-    character.charCodeAt(0) < 65
-      ? String.fromCharCode(character.charCodeAt(0) + 50)
-      : character
-  );
-  return convertedChars.join("");
-};
